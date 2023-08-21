4fd7dff4fa6d8f5ad7c900622ee640c355854130
Abhi <aanand@argusoft.com>
2023-08-21 18:45:58 +0530

    Enum

--- Hello.java
+++ Hello.java
@@ -1912,3 +1912,43 @@
 //         return 100.0;
 //     }
 // }
+
+// ENUM
+
+
+// public enum DayOfTheWeek {
+
+//     SUN, MON, TUES, WED, THURS, FRI, SAT
+// }
+
+// import java.util.Random;
+
+// public class Main {
+
+//     public static void main(String[] args) {
+
+//         DayOfTheWeek weekDay = DayOfTheWeek.TUES;
+//         System.out.println(weekDay);
+
+//         for (int i = 0; i < 10; i++ ) {
+//             weekDay = getRandomDay();
+
+//             System.out.printf("Name is %s, Ordinal Value = %d%n",
+//                     weekDay.name(), weekDay.ordinal());
+
+//             if (weekDay == DayOfTheWeek.FRI) {
+//                 System.out.println("Found a Friday!!!");
+//             }
+
+//         }
+//     }
+
+//     public static DayOfTheWeek getRandomDay() {
+
+//         int randomInteger = new Random().nextInt(7);
+//         var allDays = DayOfTheWeek.values();
+
+//         return allDays[randomInteger];
+//     }
+
+// }
